518725480392e50c9df28be47c874613c350bdcd
Svetlana Dolgopolova <svetnt75@gmail.com>
2022-12-18 11:23:06 +0500

    просто пробую

--- src/test/java/TestBoxTests.java
+++ src/test/java/TestBoxTests.java
@@ -1,18 +1,24 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TestBoxTests {
 
-   @Test
-    void fillFormTest(){
+    @BeforeAll
+    static void beforeAll(){
 
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+    }
+
+   @Test
+    void fillFormTest(){
 
         open("/text-box");
 
@@ -23,6 +29,8 @@ public class TestBoxTests {
         $("#submit").click();
 
         $("#output").shouldBe(Condition.visible);
+        $("#output #name").shouldHave(text("Murisa"));
+        $("#output #email ").shouldHave(text("mura@kisa.com"));
     }
 
 
